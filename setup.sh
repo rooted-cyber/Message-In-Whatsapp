b64fc7e050e445d98b249f44f21e219cddfc37df
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2019-11-11 09:55:57 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ apt upgrade
 apt install figlet
 apt install toilet
 apt install termux-api
-cp -f whatsapp $PREFIX/bin
-chmod 777 $PREFIX/bin/whatsapp
+cp -f messanger $PREFIX/bin
+chmod 777 $PREFIX/bin/messanger
 clear
 echo "Install termux-api app from play store"
 echo
-echo -e "\033[93m Now you can start this command :- whatsapp"
+echo -e "\033[93m Now you can start this command :- messanger"
 echo
 echo "Press enter for start"
 read
-whatsapp
+messanger
